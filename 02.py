15294f4c74f52aa413a53eb94f351f0fcbc43acf
SRRatul <120864928+SRRatul@users.noreply.github.com>
2024-06-20 11:58:18 +0600

    Zeros Matrix

---
+++ 02.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 20 11:53:14 2024
+
+@author: Salvir Rahman Ratul
+zeros matrixes
+"""
+import numpy as np
+matrix = np.zeros((2,2))
+print(matrix)
+matrix2 = np.zeros((3,3))
+print(matrix2)
+matrix3 = np.zeros((2,3))
+print(matrix3)
