0700b0d7a9525a14476c0606c79809bcea1fcc0b
SRRatul <120864928+SRRatul@users.noreply.github.com>
2024-06-20 12:20:23 +0600

    Identity Matrix

---
+++ 04.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 20 12:09:13 2024
+
+@author: Salvir Rahman Ratul
+Identity Matrix
+"""
+
+import numpy as np
+
+identity_mat = np.eye(4)
+print(identity_mat)
+identtiy_mat = np.eye(3) + np.ones(3)
+print(identtiy_mat)
