70e533a587ed0be9e2743ca17abb29dff7a85309
SRRatul <120864928+SRRatul@users.noreply.github.com>
2024-06-20 10:25:32 +0600

    Add two 2x2 matrices.

---
+++ 01A.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 20 10:08:58 2024
+
+@author: Salvir Rahman Ratul
+
+Add two 2x2 matrices.
+"""
+import numpy as np
+arr1 = np.array([[23, 45],[5, 8]])
+arr2 = np.array([[3, 6],[-4, 8]])
+result = arr1 + arr2
+print("Result : ",result)
